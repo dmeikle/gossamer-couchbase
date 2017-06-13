4a6dc1514ab7ef052e1835fff517974a3f21b7c0
Dave Meikle <davemeikle@ymail.com>
2017-06-12 22:00:25 -0700

    reworked masterlist command

--- src/Gossamer/Couchbase/Client/Commands/CouchbaseMasterListCommand.php
+++ src/Gossamer/Couchbase/Client/Commands/CouchbaseMasterListCommand.php
@@ -9,34 +9,33 @@
 namespace Gossamer\Couchbase\Client\Commands;
 
 
-class CouchbaseMasterListCommand extends AbstractCouchbaseListCommand
+class CouchbaseMasterListCommand extends BaseBucketCommand
 {
-    public function execute($params = array(), $request = array())
-    {
+
+    /**
+     * executes code specific to the child class
+     *
+     * @param array     URI params
+     * @param array     POST params
+     */
+    public function execute($params = array(), $request = array()) {
         $queryString = "SELECT " . $this->getFields() . " FROM `" . $this->getMasterBucketName() .
             "` as " . $this->entity->getClassName() . " WHERE type ='" . $this->entity->getIdentityField() . "' AND isActive = '1' " .
             $this->getFilter($params) . $this->getOrderBy($params, 'id') .
             $this->getLimit($params);
 
-        $query = \CouchbaseN1qlQuery::fromString($queryString);
+        $rows = $this->query($queryString);
 
-        $rows = $this->getBucket()->query($query);
+        $rowCount = $this->getTotalRowCount($params);
 
-        $this->httpRequest->setAttribute($this->entity->getIdentityField(),  $this->removeRowHeadings($this->resultsToArray($rows)));
-        $this->getTotalRowCount($params);
+        return  array($this->entity->getIdentityField() => $rows, $this->entity->getIdentityField() . 'Count' => $rowCount);
     }
 
-    public function getTotalRowCount($params = array(), $request = array())
-    {
+    public function getTotalRowCount($params = array(), $request = array()) {
         $queryString = "SELECT count('id') as rowCount FROM `" . $this->getMasterBucketName() .
             "` WHERE type ='" . $this->entity->getIdentityField() . "' AND isActive = '1' " .
             $this->getFilter($params);
 
-        $query = \CouchbaseN1qlQuery::fromString($queryString);
-
-        $rows = $this->getBucket()->query($query);
-
-        $this->httpRequest->setAttribute($this->entity->getIdentityField(). 'Count',  $this->resultsToArray($rows));
-
+        return $this->query($queryString);
     }
 }
